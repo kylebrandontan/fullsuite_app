19f5fd83d28da25a8cc84fcf9916010e1db01ccc
Kyle Tan <kylebrandontan@gmail.com>
2020-03-15 23:55:14 +0800

    Add Stock model and spec with association

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+
   validates :name, presence: true
   validates :sku, presence: true
   validates :price, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_15_130005) do
+ActiveRecord::Schema.define(version: 2020_03_15_155245) do
 
   create_table "products", force: :cascade do |t|
     t.string "sku"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2020_03_15_130005) do
     t.decimal "cost_of_goods"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "stocks", force: :cascade do |t|
+    t.integer "count"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_stocks_on_product_id"
   end
 
 end
