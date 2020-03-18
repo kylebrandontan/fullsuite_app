31078c777e3de8282efd2ebffbb1787f442daa9f
Kyle Tan <kylebrandontan@gmail.com>
2020-03-18 23:48:37 +0800

    Add selenium for headless testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,8 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 require 'support/factory_bot'
 require 'webdrivers/chromedriver'
+require 'support/capybara'
+require 'support/auth_helper'
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -74,6 +76,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  # config.include Devise::Test::IntegrationHelpers, type: :system
-  # config.include AuthHelper, type: :system
+  config.include Devise::Test::IntegrationHelpers, type: :system
+  config.include AuthHelper, type: :system
 end
